e73a7f5bc7d7a4bbdece3414246fef98fc6ad0e4
Shafira <shafiraaks@gmail.com>
2023-01-20 09:33:25 +0700

    update environment prod

--- angular-auth/src/environments/environment.prod.ts
+++ angular-auth/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB6KuztUDJ7dESSBDV42DqI2GFz_aFqpsY',
+    authDomain: 'angular-auth-c0d04.firebaseapp.com',
+    databaseURL: 'https://angular-auth-c0d04-default-rtdb.firebaseio.com',
+    projectId: 'angular-auth-c0d04',
+    storageBucket: 'angular-auth-c0d04.appspot.com',
+    messagingSenderId: '672782359178',
+    appId: '1:672782359178:web:56092009d46a7f03ca2b73',
+  },
 };
